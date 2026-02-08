2966308f65a018eeaaef660790bf9b71cb1759e5
masafumi yamamoto <ureuzy@gmail.com>
2026-02-08 20:29:44 +0900

    fix: restrict CORS allow origin to ureuzy.io

--- common-api/middleware.go
+++ common-api/middleware.go
@@ -45,7 +45,7 @@ func AuthMiddleware(app *firebase.App) gin.HandlerFunc {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://ureuzy.io")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
